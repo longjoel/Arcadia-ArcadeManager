3db7a4eca7ab148436fac9dd93d3854fafc0a1db
Joel Longanecker <joel.longanecker@gmail.com>
2025-06-02 20:27:33 +0000

    good enough for now.

---
+++ Edge.cs
@@ -0,0 +1,15 @@
+namespace Arcadia
+{
+    public class Edge
+    {
+        private EdgeOptions _options;
+
+        public Edge(EdgeOptions options)
+        {
+            _options = options;
+        }
+
+        
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Arcadia
 {
-    
-    
+    public enum ProgramMode
+    {
+        Edge = 0,
+        Server = 1,
+        Console = 2,
+    }
 
+    public class EdgeOptions
+    {
+        public Uri? ServerHost { get; set; } = new Uri("http://localhost:8080");
+    }
+    public class ProgramOptions
+    {
+        // Take the default value, then from the environment, override with command line args
+
+
+
+
+        /// <summary>
+        /// Program Mode: How the program should run.
+        /// /// - Edge: Runs in Edge mode, connecting to a server.
+        /// /// - Server: Runs as a server.
+        /// /// - Console: Runs in console mode, suitable for local execution.
+        /// </summary>
+
+        public ProgramMode Mode { get; set; } = ProgramMode.Console;
+
+
+        public EdgeOptions? Edge { get; set; } = null;
+
+
+    }
     public class Program
     {
+
+        ProgramOptions _options;
+        public Program(ProgramOptions options)
+        {
+            // Constructor logic can go here if needed
+            _options = options;
+        }
+
+        public static ProgramOptions ParseArgs(string[] args)
+        {
+            // Here you can implement argument parsing logic
+            // For now, we will just return a new instance of ProgramOptions
+            return new ProgramOptions();
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("This is a test of the Arcadia project.");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+
         }
 
     }
